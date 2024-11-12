cc85634993ad5c2783ea0d7cf71b2d8bea3fe834
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2024-11-12 17:18:09 -0300

    Implement selector

--- src/server/echoServer.c
+++ src/server/echoServer.c
@@ -2,9 +2,76 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "selector.h"
 
 #define READ_BUFFER_SIZE 2048
 #define MAX_HOSTNAME_LENGTH 255
+
+//TODO
+/* declaración forward de los handlers de selección de una conexión
+ * establecida entre un cliente y el proxy.
+ */
+static void socksv5_read   (struct selector_key *key);
+static void socksv5_write  (struct selector_key *key);
+static void socksv5_block  (struct selector_key *key);
+static void socksv5_close  (struct selector_key *key);
+static const struct fd_handler echo_handler = {
+  .handle_read   = socksv5_read,
+  .handle_write  = socksv5_write,
+  .handle_close  = socksv5_close,
+  .handle_block  = socksv5_block,
+};
+
+//TODO
+void pool_destroy(void) {
+  struct socks5 *next, *s;
+  for(s = pool; s != NULL ; s = next) {
+    next = s->next;
+    free(s);
+  }
+}
+
+
+
+void passive_accept(struct selector_key * key) {
+  struct sockaddr_storage       client_addr;
+  socklen_t                     client_addr_len = sizeof(client_addr);
+  struct socks5                 *state           = NULL;
+
+  const int client = accept(key->fd, (struct sockaddr*) &client_addr,
+                                                        &client_addr_len);
+  if(client == -1) {
+    goto fail;
+  }
+  if(selector_fd_set_nio(client) == -1) {
+    goto fail;
+  }
+
+  //TODO
+  state = socks5_new(client);
+  if(state == NULL) {
+    // sin un estado, nos es imposible manejaro.
+    // tal vez deberiamos apagar accept() hasta que detectemos
+    // que se liberó alguna conexión.
+    goto fail;
+  }
+  memcpy(&state->client_addr, &client_addr, client_addr_len);
+  state->client_addr_len = client_addr_len;
+
+  if(SELECTOR_SUCCESS != selector_register(key->s, client, &socks5_handler,
+                                            OP_READ, state)) {
+    goto fail;
+                                            }
+  return ;
+  fail:
+      if(client != -1) {
+        close(client);
+      }
+  socks5_destroy(state);
+}
 
 int handleEchoClient(const int clientFd)
 {
--- src/server/echoServer.h
+++ src/server/echoServer.h
@@ -1,6 +1,9 @@
 #ifndef ECHOSERVER_H
 #define ECHOSERVER_H
 
+void passive_accept(struct selector_key *key);
+void pool_destroy(void);
 int handleEchoClient(int clientFd);
+
 
 #endif //ECHOSERVER_H
