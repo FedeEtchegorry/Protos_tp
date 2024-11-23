9c82aae92e09afcabd3a09a4d6b0c62149287ab6
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-11-23 16:09:05 -0300

    now parsers can be used depending if it's user or manager
    Co-Authored-By: ivanOdz <76888044+ivanOdz@users.noreply.github.com>
    Co-Authored-By: tomaScheffer <108834945+tomaScheffer@users.noreply.github.com>

--- src/server/managerParser.c
+++ src/server/managerParser.c
@@ -1,1 +1,11 @@
 #include "managerParser.h"
+
+static const methodsMap manager_methods[] = {
+    {"USER", USER}, {"PASS", PASS},
+    {"DATA", DATA},{"QUIT", QUIT},{NULL, UNKNOWN}
+};
+
+
+const methodsMap* getManagerMethods(){
+  return manager_methods;
+}
